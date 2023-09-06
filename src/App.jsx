bf6bb97f96af4cbd79efafb1f8925f5df7724fa0
AlexandraM <76844097+Alexandra2888@users.noreply.github.com>
2023-09-06 11:19:11 +0300

    adding pages structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,18 @@
+import {
+  HomeLayout,
+  Landing,
+  Error,
+  Products,
+  SingleProduct,
+  Cart,
+  About,
+  Register,
+  Login,
+  Checkout,
+  Orders,
+} from './pages';
+
+
 const App = () => {
   return <h1 className='text-7xl font-bold underline'>Tailwind project</h1>;
 };
