ec80c7d9924c2f5dd2354c12fa7d478967c2a6c8
AlexandraM <76844097+Alexandra2888@users.noreply.github.com>
2023-09-15 12:44:35 +0300

    refactor hello.js

--- functions/hello.js
+++ functions/hello.js
@@ -1,6 +1,11 @@
-exports.handler = async function(event, context) {
+const items = [
+    { id: 1, name: 'Jake' },
+    { id: 2, name: 'Amy' },
+  ]
+  
+  exports.handler = async function (event, context) {
     return {
-        statusCode: 200,
-        body: 'Hello world'
+      statusCode: 200,
+      body: 'hello world',
     }
-}
+  }
