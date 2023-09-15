c0e3d3c1d97b1493dc9c30f609438529d5fe0639
AlexandraM <76844097+Alexandra2888@users.noreply.github.com>
2023-09-15 12:11:38 +0300

    finishing project

--- src/pages/HomeLayout.jsx
+++ src/pages/HomeLayout.jsx
@@ -1,16 +1,19 @@
 import { Outlet, useNavigation } from 'react-router-dom';
-import { Header, Navbar} from '../components'; 
-
-
-
+import { Header, Navbar, Loading } from '../components';
 const HomeLayout = () => {
+  const navigation = useNavigation();
+  const isPageLoading = navigation.state === 'loading';
   return (
     <>
       <Header />
       <Navbar />
+      {isPageLoading ? (
+        <Loading />
+      ) : (
         <section className='align-element py-20'>
           <Outlet />
         </section>
+      )}
     </>
   );
 };
--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,24 +1,26 @@
-import React from 'react';
-import { FeaturedProducts, Hero } from "./../components";
+import { FeaturedProducts, Hero } from '../components';
 
 import { customFetch } from '../utils';
 const url = '/products?featured=true';
 
-export const loader = async () => {
-  const response = await customFetch(url);
-  console.log(response);
+const featuredProductsQuery = {
+  queryKey: ['featuredProducts'],
+  queryFn: () => customFetch(url),
+};
+
+export const loader = (queryClient) => async () => {
+  const response = await queryClient.ensureQueryData(featuredProductsQuery);
+
   const products = response.data.data;
   return { products };
 };
 
-
 const Landing = () => {
   return (
- <>
- <Hero/>
- <FeaturedProducts/>
- </>
-  )
-}
-
-export default Landing
+    <>
+      <Hero />
+      <FeaturedProducts />
+    </>
+  );
+};
+export default Landing;
